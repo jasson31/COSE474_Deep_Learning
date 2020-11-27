c26b21a39ce768cb4a5a8931e1aa154e4a35c336
piicaterpillar <zzdldl31@gmail.com>
2020-11-27 21:06:32 +0900

    인풋 형식 변경

--- yacht_main.py
+++ yacht_main.py
@@ -3,13 +3,14 @@ import yacht_score
 from yacht_score import score
 import math
 
-score_board = [[-10]*12]*2
+score_board = [[0]*12]*2
 score_func = [yacht_score.ONES, yacht_score.TWOS, yacht_score.THREES, yacht_score.FOURS, yacht_score.FIVES,
               yacht_score.SIXES, yacht_score.CHOICE, yacht_score.FOUR_OF_A_KIND, yacht_score.FULL_HOUSE,
               yacht_score.SMALL_STRAIGHT, yacht_score.LARGE_STRAIGHT, yacht_score.YACHT]
 dice_status = [randint(1, 6), randint(1, 6), randint(1, 6), randint(1, 6), randint(1, 6)]
 roll_count = 2
-total_score = [0, 0]
+score_total = [0, 0]
+bonus_total = [0, 0]
 cur_player = 0
 multi_mode = False
 
@@ -31,15 +32,11 @@ def dice_status_output():
     return l
 
 def get_yacht_output():
-    cur_total_score = 0
-    for i in range(len(score_board[cur_player])):
-        cur_total_score += score_board[cur_player][i]
-        #if score_board[cur_player][i] == -10:
-        #    cur_total_score += 10
+    
     if multi_mode:
-        return score_board[cur_player] + score_board[int(not cur_player)] + [roll_count] + dice_status_output(), cur_total_score, is_game_finished()
+        return score_board[cur_player] + score_board[int(not cur_player)] + [roll_count] + dice_status_output()+bonus_total, score_total, is_game_finished()
     else:
-        return score_board[cur_player] + [roll_count] + dice_status_output(), cur_total_score, is_game_finished()
+        return score_board[cur_player] + [roll_count] + dice_status_output() + [bonus_total[cur_player]], score_total[cur_player], is_game_finished()
 
 
 def set_multi_mode(mode):
@@ -48,24 +45,29 @@ def set_multi_mode(mode):
 
 
 def set_score(dice, category):
-    score_board[cur_player][category] = score(dice, score_func[category])
-    return score(dice, score_func[category])
+    added_score = score(dice, score_func[category])
+    
+    score_board[cur_player][category] = 1
+    score_total[cur_player] += added_score
+    if category<6:
+        bonus_total[cur_player] += added_score
+    return
 
 
 def is_game_finished():
-    return -10 not in score_board[cur_player]
+    return 0 not in score_board[cur_player]
 
 def cheated():
     for i in range(len(score_board[cur_player])):
-        if score_board[cur_player][i] == -10:
-            score_board[cur_player][i] = -11
+        if score_board[cur_player][i] == 0:
+            score_board[cur_player][i] = -1
 
 
 def update(command_index):
-    global roll_count, total_score, cur_player
+    global roll_count, score_total, cur_player
     if is_game_finished():
         #print('Game End')
-        #print('player : ', cur_player, ' total score : ', total_score[cur_player])
+        #print('player : ', cur_player, ' total score : ', score_total[cur_player])
         if multi_mode:
             cur_player = int(not cur_player)
     else:
@@ -74,57 +76,37 @@ def update(command_index):
         if command_index < 31:
             if roll_count == 0:
                 cheated()
-                return -100
+                return
             else:
                 roll_dice(command_index+1)
-                max_estimated_score = 0
-                for i in range(len(score_board[cur_player])):
-                    if score_board[cur_player][i] == -10:
-                        temp_score = score(dice_status, score_func[i])
-                        if temp_score > max_estimated_score:
-                            max_estimated_score = temp_score
-                return max_estimated_score
+                return
         else:
-            is_cheated, cur_set_score = False, 0
-
             score_index = command_index - 31
             #print(str(score_index))
-            if score_board[cur_player][score_index] != -10:
+            if score_board[cur_player][score_index] != 0:
                 cheated()
-                is_cheated = True
             else:
-                # Set score
-                #print('player : ', cur_player, ' Set score')
-                cur_set_score = set_score(dice_status, score_index)
-            
+                set_score(dice_status, score_index)
             
             roll_count = 3
             roll_dice(31)
+            
             if is_game_finished():  # Game Ended
-                #print('Game End')
-                bonus_counter, score_sum = 0, 0
-                for i in score_board[cur_player]:
-                    score_sum += i
-                    if i < 6:
-                        bonus_counter += i
-                if bonus_counter >= 63:
-                    score_sum += 35
-                total_score[cur_player] = score_sum
-                #print('player : ', cur_player, ' total score : ', score_sum)
 
+                if bonus_total[cur_player] >= 63:
+                    score_total[cur_player] += 35
+                
+                #print('player : ', cur_player, ' total score : ', score_sum)
             if multi_mode:
                 cur_player = int(not cur_player)
 
-            if is_cheated:
-                return -100
-            else:
-                return cur_set_score
 
 
 def reset_game():
-    global score_board, dice_status, roll_count, total_score, cur_player
-    score_board = [[-10]*12]*2
+    global score_board, dice_status, roll_count, score_total, cur_player
+    score_board = [[0]*12]*2
     dice_status = [randint(1, 6), randint(1, 6), randint(1, 6), randint(1, 6), randint(1, 6)]
     roll_count = 2
-    total_score = [0, 0]
+    score_total = [0, 0]
+    bonus_total = [0, 0]
     cur_player = 0
