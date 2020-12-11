4800f1da9987f76e4472fdf676bd4f2eb4d7ad7d
RyuJiSeok <roadmageb@naver.com>
2020-12-11 13:45:56 +0900

    episode learning

--- yacht_main.py
+++ yacht_main.py
@@ -130,14 +130,16 @@ def get_yacht_output():
         for i in range(len(score_board_output)):
             if score_board[cur_player][i] == 0:
                 score_board_output[i] = -10
-            else:
+            elif score_board[cur_player][i] == 1:
                 score_board_output[i] = yacht_score.score(dice_status, score_func[i])
+            else:
+                score_board_output[i] = -1
 
-        print(score_board_output)
+        
 
-        '''return score_board_output + [roll_count] + dice_status\
+        return score_board_output + [roll_count] + dice_status\
                 + [bonus_total[cur_player] / 126]+ [score_total[cur_player] / 350],\
-                  score_total[cur_player], is_game_finished(), get_available_input()'''
+                  score_total[cur_player], is_game_finished(), get_available_input()
 
 
 def set_multi_mode(mode):
